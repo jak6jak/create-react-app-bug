fd072d8977974355482c6908b2b7f97b10139dac
Jacob Edie <jacobedie1@gmail.com>
2021-01-21 11:02:49 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books = [
+  {
+    id: 1,
+    img: "https://prodimage.images-bn.com/pimages/9781680105223_p0_v2_s550x406.jpg",
+    title: "I love you to the moon and back",
+    author: "Jacob Edie"
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
+    title: "Our Class is a Family",
+    author: "Shannon Olsen"
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51cApiT7xML._SX327_BO1,204,203,200_.jpg",
+    title: "All the Ugly and wonderful Things: A Novel",
+    author: "Bryn Greenwood"
+  }
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} book={...book}></Book>
+      })}
+    </section>
+  );
+}
+
+
+const Book = (props) => {
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+
+  );
+}
+
+ReactDom.render(<BookList />, document.getElementById('root'))
